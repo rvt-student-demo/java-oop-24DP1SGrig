0c4de6963fa5765a9245d30ae1b9e59fe9f30d4d
Sergejs Grigorjevs <A240026SG@rvt.lv>
2026-02-11 15:24:36 +0200

    Nepabeigata registracija

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,16 +1,9 @@
 package rvt;
 
-import java.util.Scanner;
-
 
 public class App {
     public static void main(String[] args) {
-        TodoList list = new TodoList();
-        // ...existing code...
-        try (Scanner scanner = new Scanner(System.in)) {
-            UserInterface ui = new UserInterface(list, scanner);
-            ui.start();
-        }
+        System.out.printf("%20s", "12345");
         
     }
 }
---
+++ src/main/java/rvt/StudentuRegistracija/Students.java
@@ -0,0 +1,15 @@
+package rvt.StudentuRegistracija;
+
+import java.sql.Time;
+import java.util.Date;
+
+public class Students {
+    private String Vards;
+    private String Uzvards;
+    private String Epasts;
+    private int PersonasKods;
+    private Date RegDatums;
+    private Time RegLaiks;
+    
+    
+}
