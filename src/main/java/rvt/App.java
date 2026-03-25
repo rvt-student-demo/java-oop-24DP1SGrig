73d98d680adb7b79aa81364623d3fdc53a27327d
Sergejs Grigorjevs <A240026SG@rvt.lv>
2026-03-25 11:23:27 +0200

    Online shop

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,13 +1,23 @@
 package rvt;
 
+import java.util.Scanner;
+
+import OnlineShop.Store;
+import OnlineShop.Warehouse;
+
 public class App {
     public static void main(String[] args) {
-        IOU mattsIOU = new IOU();
-        mattsIOU.setSum("Arthur", 51.5);
-        mattsIOU.setSum("Michael", 30);
+        Warehouse warehouse = new Warehouse();
+        
+        warehouse.addProduct("coffee", 5, 10);
+        warehouse.addProduct("milk", 3, 20);
+        warehouse.addProduct("cream", 2, 55);
+        warehouse.addProduct("bread", 7, 8);
 
-        System.out.println(mattsIOU.howMuchDoIOweTo("Arthur"));
-        System.out.println(mattsIOU.howMuchDoIOweTo("Michael"));
+        Scanner scanner = new Scanner(System.in);
+
+        Store store = new Store(warehouse, scanner);
+        store.shop("Sergejs");
     }
     
 }
